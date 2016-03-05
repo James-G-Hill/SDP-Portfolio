617bea9e452929430d7184e93665e2521775a49d
James <jamesghill@yahoo.co.uk>
2016-03-05 20:30:46 +0000

    Moved questions file from the Strategy package.

--- Lecture 5 - More Design Patterns/src/main/scala/Questions.scala
+++ Lecture 5 - More Design Patterns/src/main/scala/Questions.scala
@@ -12,3 +12,13 @@ package Strategy
 // The strategy design pattern should be used when you need to define a
 // number of classes that differ only in their behaviour and should be
 // used interchangeably so that different algorithms can be used at runtime.
+// 
+// Q3
+// 
+// The abstract factory design pattern should be used when the application
+// needs to be able to produce distinct sets of related objects; the client
+// can interact with many different similar concrete factories to produce
+// concrete objects that it can work with; the client doesn't need to reference
+// the concrete objects as it always works through interfaces.
+// 
+// 
