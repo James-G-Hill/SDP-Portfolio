2f9eb4d008f837058d722b0905d4a853d443e840
James <jamesghill@yahoo.co.uk>
2016-03-11 22:54:06 +0000

    Created an answer for the class arguments questions.

---
+++ Lecture 6 - Functional Programming/src/main/scala/classargs/classargs.scala
@@ -0,0 +1,37 @@
+package classargs
+
+import com.atomicscala.AtomicTest._
+
+object TestArgs extends App {
+  
+  def squareThem(them: Int*): Int = {
+    var sum = 0
+    for(t <- them){sum = sum + (t * t)}
+    sum
+  }
+  
+  val family1 = new Family("Mum", "Dad", "Sally", "Dick")
+  family1.familySize() is 4
+  
+  val family2 = new Family("Dad", "Mom", "Harry")
+  family2.familySize() is 3
+  
+  val family3 = new FlexibleFamily("Mum", "Dad", "Sally", "Dick")
+  family3.familySize() is 4
+  
+  val family4 = new FlexibleFamily("Dad", "Mom", "Harry")
+  family4.familySize() is 3
+  
+  squareThem(2) is 4
+  squareThem(2, 4) is 20
+  squareThem(1, 2, 4) is 21
+  
+}
+
+class Family(name: String*) {
+  def familySize() = name.size
+}
+
+class FlexibleFamily(parent1: String, parent2: String, children: String*) {
+  def familySize() = 2 + children.size
+}
