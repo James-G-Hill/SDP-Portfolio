7e2a697f4bfb9fe246029a33cbbfd9eea1bd3585
James <jamesghill@yahoo.co.uk>
2016-03-12 20:07:12 +0000

    Completed questions for auxiliary constructors.

---
+++ Lecture 6 - Functional Programming/src/main/scala/auxillaryconstructors/clothesWashers.scala
@@ -0,0 +1,8 @@
+package auxillaryconstructors
+
+class ClothesWasher(modelName:String, capacity:Double) {  
+  def this(modelName: String){this(modelName, 20.5)}
+  def this(capacity: Double){this("M", capacity)}
+}
+
+class ClothesWasher2(modelName:String = "M", capacity:Double = 20.5)
