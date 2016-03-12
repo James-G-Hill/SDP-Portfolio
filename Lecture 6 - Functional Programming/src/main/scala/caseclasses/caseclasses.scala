f8256f0163d652c68c31d18abc33f12cc6e275d0
James <jamesghill@yahoo.co.uk>
2016-03-12 20:53:23 +0000

    Completed questions for case classes.

---
+++ Lecture 6 - Functional Programming/src/main/scala/caseclasses/caseclasses.scala
@@ -0,0 +1,24 @@
+package caseclasses
+
+import com.atomicscala.AtomicTest._
+
+object caseClassesTest extends App {
+  
+  val p = Person("Jane", "Smile", "jane@smile.com")
+  p.first is "Jane"
+  p.last is "Smile"
+  p.email is "jane@smile.com"
+  
+  val people = Vector(
+    Person("Jane", "Smile", "jane@smile.com"),
+    Person("Ron", "House", "ron@house.com"),
+    Person("Sally", "Dove", "sally@dove.com")
+  )
+  
+  people(0) is "Person(Jane,Smile,jane@smile.com)"
+  people(1) is "Person(Ron,House,ron@house.com)"
+  people(2) is "Person(Sally,Dove,sally@dove.com)"
+  
+}
+
+case class Person(first: String, last: String, email: String)
