6d31273c7465e3ba345bb097d196cb9e2980e278
James <jamesghill@yahoo.co.uk>
2016-03-12 22:03:04 +0000

    Added question 20 and question numbers.

--- Lecture 6 - Functional Programming/src/main/scala/parameterizedtypes/parameterizedtypes.scala
+++ Lecture 6 - Functional Programming/src/main/scala/parameterizedtypes/parameterizedtypes.scala
@@ -20,15 +20,23 @@ object parameterizedtypes extends App {
     v.toSet
   }
   
+  // Q.17
   inferred('a', 'b', 'c') is "Vector(a, b, c)"
   explicit('a', 'b', 'c') is "Vector(a, b, c)"
   
   explicit(1.0, 2.0, 3.0) is Vector(1.0,2.0,3.0)
   
+  // Q.18
   explicitList(Vector(10.0, 20.0)) is List(10.0, 20.0)
   explicitList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
   
+  // Q.19
   explicitSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
   explicitSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
+  
+  // Q.20
+  var str = ""
+  explicitSet(Vector(1, 2, 3, 2, 3, 4)).foreach({x => str = str + x + ","})
+  str is "1,2,3,4,"
   
 }
