1ae1f300fc1797be3b5e1994f129c0d42b14af35
James <jamesghill@yahoo.co.uk>
2016-03-12 20:53:03 +0000

    Completed questions for paramaterized types.

---
+++ Lecture 6 - Functional Programming/src/main/scala/parameterizedtypes/parameterizedtypes.scala
@@ -0,0 +1,34 @@
+package parameterizedtypes
+
+import com.atomicscala.AtomicTest._
+
+object parameterizedtypes extends App {
+  
+  def inferred(c1: Char, c2: Char, c3: Char) = {
+    Vector(c1, c2, c3)
+  }
+  
+  def explicit[T](c1: T, c2: T, c3: T): Vector[T] = {
+    Vector(c1, c2, c3)
+  }
+  
+  def explicitList[T](v: Vector[T]): List[T] = {
+    v.toList
+  }
+  
+  def explicitSet[T](v: Vector[T]): Set[T] = {
+    v.toSet
+  }
+  
+  inferred('a', 'b', 'c') is "Vector(a, b, c)"
+  explicit('a', 'b', 'c') is "Vector(a, b, c)"
+  
+  explicit(1.0, 2.0, 3.0) is Vector(1.0,2.0,3.0)
+  
+  explicitList(Vector(10.0, 20.0)) is List(10.0, 20.0)
+  explicitList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
+  
+  explicitSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
+  explicitSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
+  
+}
